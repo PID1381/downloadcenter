e94b0296e9056a504c473d323334ce402abf4813
PID1381 <c.alessandro@blu.it>
2026-05-15 15:02:48 +0200

    upgrade animeunity

--- scripts/anime/moduli/AnimeUnity/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""Modulo AnimeUnity – Download Center (upgrade-3)"""
-from .handlers_animeunity import run, search, get_episodes, cambio_url, debug_modulo
-
-__all__ = ["run", "search", "get_episodes", "cambio_url", "debug_modulo"]
